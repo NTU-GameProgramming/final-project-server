c768073e9016204a7434c96171648ba567a8c26e
meteorologytoday <meteorologytoday@gmail.com>
2014-12-27 21:58:20 +0800

    Create Client.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
 var net = require('net');
-var JsonSocket = require('./json-socket');
-var GameTree = require('./Game/game-tree');
-var ClientManager = require('./client_manager');
+var JsonSocket = require('./lib/json-socket');
+var GameTree = require('./lib/Game/game-tree');
+var ClientManager = require('./lib/client_manager');
 //var Connector = require('connector')
 
 
@@ -11,14 +11,14 @@ var port = 8976;
 
 // Create a game
 var server = net.createServer();
-console.log('This is game server.');
-console.log('Initializing game...');
+console.log('This is game server for NTU-Game_Programming-2014 Fall');
 var game_tree = new GameTree();
 
 cm = new ClientManager();
 
 
 server.on('connection', function(socket) { // socket is a standard net.Socket
+	console.log("[Server] Got connection.");
     json_socket = new JsonSocket(socket);
 	cm.newConnection(json_socket);
 /*	json_socket.sendMessage({msg: 'Hello Node.js'}, function(){
